dede92e1c3c931c47f0238f2da4aa486a93de182
Becky1703 <Becky1703>
2024-01-09 08:23:13 +0100

    added comments to store and user types

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,7 +4,7 @@ import userReducer from "./users/user.reducer";
 import { thunk } from "redux-thunk";
 import { noteReducer } from "./notes/note.reducer";
 
-
+// Combines multiple reducers into a single rootReducer
 let rootReducer = combineReducers({
 
     userReducer:userReducer,
@@ -12,4 +12,5 @@ let rootReducer = combineReducers({
     
 });
 
+// Creates the Redux store by appplying middleware (thunk) to the rootReducer
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
--- src/Redux/users/user.types.js
+++ src/Redux/users/user.types.js
@@ -1,3 +1,4 @@
+// Action types for user login and logout
 export const LOGIN_USER_LOADING = "login/user/loading"
 export const LOGIN_USER_SUCCESS= "login/user/success"
 export const LOGIN_USER_ERROR = "login/user/error"
