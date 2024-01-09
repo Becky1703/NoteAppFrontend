58f0d0915f1620ab74354c17d325670e5034b283
Becky1703 <Becky1703>
2024-01-09 08:37:29 +0100

    added some comments to user reducer function

--- src/Redux/users/user.reducer.js
+++ src/Redux/users/user.reducer.js
@@ -1,5 +1,6 @@
-import { LOGIN_USER_ERROR, LOGIN_USER_LOADING, LOGIN_USER_SUCCESS, LOGOUT, REGISTER_USER_LOADING } from "./user.types"
+import { LOGIN_USER_ERROR, LOGIN_USER_LOADING, LOGIN_USER_SUCCESS, LOGOUT } from "./user.types"
 
+// Defines the initial state for the User router
 const initialState = {
     token: null,
     auth: false,
@@ -7,13 +8,17 @@ const initialState = {
     error: false
 }
 
+// Defines user reducer function that handles various actions
 export default function userReducer(state=initialState, action) {
+    // Destructures the action object to get the type and payload
     const {type, payload} = action
 
+    // Checks the type of action and performs the corresponding action
     switch(type) {
 
         case LOGIN_USER_LOADING: {
 
+            // Returns the state with loading set to true
             return {
                 ...state, loading: true
             }
@@ -21,6 +26,7 @@ export default function userReducer(state=initialState, action) {
 
         case LOGIN_USER_SUCCESS: {
 
+            // Returns the state with loading set to false, error set to false, token set to payload, and auth set to true
             return {
                 ...state, loading: false, error: false, token: payload, auth: true
             }
@@ -29,6 +35,8 @@ export default function userReducer(state=initialState, action) {
 
         case LOGIN_USER_ERROR: {
 
+
+            // Returns the state with loading set to false, error set to true, and auth set to false
             return {
                 ...state, loading: false, error: true
             }
@@ -36,10 +44,12 @@ export default function userReducer(state=initialState, action) {
 
         
         case LOGOUT : {
+            // Returns the initial state
             return initialState
         }
 
         default: {
+            // Returns the current state objet if the action type is not recognized
             return state
         }
 
