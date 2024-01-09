01b8f61aa1047cd3d112e2d76f3604ccd2123eb0
Becky1703 <Becky1703>
2024-01-09 07:57:21 +0100

    added comments

--- src/Redux/notes/note.types.js
+++ src/Redux/notes/note.types.js
@@ -1,15 +1,19 @@
+// Action types for fetching notes
 export const GET_NOTES_LOADING = "get/notes/loading"
 export const GET_NOTES_SUCCESS = "get/notes/success"
 export const GET_NOTES_ERROR = "get/notes/error"
 
+// Action types for creating notes
 export const CREATE_NOTES_LOADING = "create/notes/loading"
 export const CREATE_NOTES_SUCCESS= "create/notes/success"
 export const CREATE_NOTES_ERROR = "create/notes/error"
 
+// Action types for updating notes
 export const UPDATE_NOTES_LOADING = "update/notes/loading"
 export const UPDATE_NOTES_SUCCESS= "update/notes/success"
 export const UPDATE_NOTES_ERROR = "update/notes/error"
 
+// Action types for deleting notes
 export const DELETE_NOTES_LOADING = "delete/notes/loading"
 export const DELETE_NOTES_SUCCESS= "delete/notes/success"
 export const DELETE_NOTES_ERROR = "delete/notes/error"
